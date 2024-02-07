fef12fe2db52bd32859f79160f01098e1e2afc12
Alejandro Sánchez Fernández <alexius1996@gmail.com>
2024-02-07 21:11:55 +0100

    update

--- codedeploy-scripts/validation.sh
+++ codedeploy-scripts/validation.sh
@@ -3,6 +3,9 @@
 # Obtener el endpoint de SSM
 ENDPOINT=$(cat /tmp/endpoint.txt)
 
+# Esperar 30 segundos
+sleep 30
+
 # Probar el endpoint
 if wget --spider -q "$ENDPOINT"; then
     echo "Endpoint is working"
